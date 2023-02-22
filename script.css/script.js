83e7b3bc688ae6623dcd998c336409749427835c
Litvac Paul <litvacpaul@MacBook-Air-Litvac.local>
2023-02-23 01:25:41 +0200

    project done

--- script.css/script.js
+++ script.css/script.js
@@ -1,13 +1,16 @@
-const popup = document.querySelector(".popup");
-const popupContainer = document.querySelector(".popup__container");
-const editProfileButton = document.querySelector(".profile__info-button");
-const grid = document.querySelector(".grid-net");
-const hearts = grid.querySelectorAll(".grid-net__item-button");
+let popup = document.querySelector(".popup");
+let popupContainer = document.querySelector(".popup__container");
+let editProfileButton = document.querySelector(".profile__info-button");
+let grid = document.querySelector(".grid-net");
+let hearts = document.querySelectorAll(".grid-net__item-button");
 editProfileButton.addEventListener("click", popupEdit);
 
 function popupEdit() {
  popup.classList.add("popup_opened");
+ popup.style.removeProperty('visibility');
 }
+
+console.log(popup);
 
 hearts.forEach((element) => 
 element.addEventListener('click', (evt) => {
@@ -15,18 +18,18 @@ element.addEventListener('click', (evt) => {
     evt.target.classList.toggle('grid-net__item-button')
   }
 ));
-
-const closeProfileButton = document.querySelector(".popup__close-button");
+console.log(popup);
+let closeProfileButton = document.querySelector(".popup__close-button");
 closeProfileButton.addEventListener("click", popupClosed);
-
+console.log(popup);
 function popupClosed() {
  popup.classList.remove("popup_opened");
 }
+console.log(popup);
+let userName = document.querySelector(".profile__info-fullname");
+let occupation = document.querySelector(".profile__info-occupation");
 
-const userName = document.querySelector(".profile__info-fullname");
-const occupation = document.querySelector(".profile__info-occupation");
-
-const formElement = document.querySelector(".popup__form");
+let formElement = document.querySelector(".popup__form");
 let nameInput = document.querySelector(".popup__username");
 nameInput.value = userName.textContent;
 let jobInput = document.querySelector(".popup__occupation");
