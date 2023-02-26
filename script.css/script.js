ea997da3b8c6338b179f7edcedb9ff4fb07f4545
Litvac Paul <litvacpaul@MacBook-Air-Litvac.local>
2023-02-26 19:33:03 +0200

    sokjdsf

--- script.css/script.js
+++ script.css/script.js
@@ -1,39 +1,32 @@
-let popup = document.querySelector(".popup");
-let popupContainer = document.querySelector(".popup__container");
-let editProfileButton = document.querySelector(".profile__info-button");
+let popup = document.querySelector('.popup');
+let popupContainer = document.querySelector('.popup__container');
+let editProfileButton = document.querySelector('.profile__info-button');
 let grid = document.querySelector(".grid-net");
-let hearts = document.querySelectorAll(".grid-net__item-button");
-editProfileButton.addEventListener("click", popupEdit);
+let hearts = document.querySelectorAll('.grid-net__item-button');
+let userName = document.querySelector('.profile__info-fullname');
+let occupation = document.querySelector('.profile__info-occupation');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__username');
+let jobInput = document.querySelector('.popup__occupation');
+let closeProfileButton = document.querySelector('.popup__close-button');
+editProfileButton.addEventListener('click', popupEdit);
 
 function popupEdit() {
- popup.classList.add("popup_opened");
- popup.style.removeProperty('visibility');
+ popup.classList.add('popup_opened');
+ nameInput.value = userName.textContent;
+ jobInput.value = occupation.textContent;
 }
-
-console.log(popup);
 
 hearts.forEach((element) => 
 element.addEventListener('click', (evt) => {
-    evt.target.classList.toggle('grid-net__item-button_active');
-    evt.target.classList.toggle('grid-net__item-button')
+    evt.target.classList.add('grid-net__item-button_active');
   }
 ));
-console.log(popup);
-let closeProfileButton = document.querySelector(".popup__close-button");
-closeProfileButton.addEventListener("click", popupClosed);
-console.log(popup);
-function popupClosed() {
- popup.classList.remove("popup_opened");
-}
-console.log(popup);
-let userName = document.querySelector(".profile__info-fullname");
-let occupation = document.querySelector(".profile__info-occupation");
 
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__username");
-nameInput.value = userName.textContent;
-let jobInput = document.querySelector(".popup__occupation");
-jobInput.value = occupation.textContent;
+closeProfileButton.addEventListener('click', popupClosed);
+function popupClosed() {
+ popup.classList.remove('popup_opened');
+}
 
 function handleFormSubmit(evt) {
  evt.preventDefault();
@@ -42,4 +35,4 @@ function handleFormSubmit(evt) {
  popupClosed();
 }
 
-formElement.addEventListener("submit", handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);
